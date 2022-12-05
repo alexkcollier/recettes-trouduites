4be03f32ec1b3f26eab582e0b62b71a993e83473
Alex Collier <alexkcollier@gmail.com>
2022-12-04 21:11:22 -0500

    Installs eslint, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: ['prettier'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:astro/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.eslint.json',
+    extraFileExtensions: ['.astro'],
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  ],
+};
