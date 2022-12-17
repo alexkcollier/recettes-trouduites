0c6f59b5e7edfb6f7a9f6ffcbaf06cca0242138e
Alex Collier <alexkcollier@gmail.com>
2022-12-17 11:45:36 -0500

    Adds post list components

---
+++ src/components/RecipePostItem.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let url: string;
+  export let label: string;
+  export let date: string;
+
+  const formatter = new Intl.DateTimeFormat(['fr-CA'], {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const dateValue = new Date(date);
+</script>
+
+<article>
+  <h1>
+    <a href={url}>{label}</a>
+  </h1>
+
+  <footer>
+    {formatter.format(dateValue)}
+  </footer>
+</article>
+
+<style>
+  h1 {
+    font-size: 1.5rem;
+    margin-bottom: 0;
+  }
+
+  footer {
+    color: rgba(0 0 0 / 0.56);
+  }
+</style>
---
+++ src/components/RecipePostList.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import RecipePostItem from '@components/RecipePostItem.svelte';
+  import type { MarkdownFrontmatter } from '@layouts/MarkdownFrontmatter';
+  import type { MarkdownInstance } from 'astro';
+
+  export let posts: readonly MarkdownInstance<MarkdownFrontmatter>[];
+</script>
+
+<ul>
+  {#each posts as post}
+    {#if post.url}
+      <li>
+        <RecipePostItem
+          url={post.url}
+          label={post.frontmatter.title}
+          date={post.frontmatter.date}
+        />
+      </li>
+    {/if}
+  {/each}
+</ul>
