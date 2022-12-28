6b7a6e05bae877fd07584b7c6b94601a24477790
Alex Collier <alexkcollier@gmail.com>
2022-12-28 16:12:44 -0500

    Adds `netlify-cms`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,64 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+          repo: 'alexkcollier/recettes-trouduites',
+        },
+        collections: [
+          {
+            name: 'recettes',
+            label: 'Recette',
+            folder: 'src/pages/recettes',
+            create: true,
+            delete: true,
+            fields: [
+              {
+                name: 'layout',
+                label: 'layout',
+                widget: 'hidden',
+                default: '@layouts/MarkdownLayout.astro',
+              },
+              {
+                name: 'title',
+                widget: 'string',
+                label: 'Title',
+              },
+              {
+                name: 'date',
+                widget: 'date',
+                label: 'Date',
+                date_format: 'YYYY-MM-DD',
+              },
+              {
+                name: 'sourceUrl',
+                widget: 'string',
+                label: 'Source URL',
+                required: false,
+              },
+              {
+                name: 'sourceText',
+                widget: 'string',
+                label: 'Source Text',
+                required: false,
+              },
+              {
+                name: 'body',
+                widget: 'markdown',
+                label: 'Body',
+              },
+            ],
+          },
+        ],
+      },
+    }),
+    svelte(),
+  ],
 });
